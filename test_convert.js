b383a1cdad074cff6ddbc3a63ca8e376e1043a8e
StageGuard <sg@mamoe.net>
2026-01-08 22:51:06 +0800

    parse logic rule

--- test_convert.js
+++ test_convert.js
@@ -1,24 +1,15 @@
-const fs = require('fs');
-const yaml = require('js-yaml');
 const { convert } = require('./lib/converter');
 
-const templatePath = 'docs/mihomo_config_template.yaml';
+const sampleConfig = {
+    proxies: [],
+    'proxy-groups': [],
+    rules: [
+        'DOMAIN-SUFFIX,google.com,Proxy',
+        'AND,((DOMAIN,baidu.com),(NETWORK,UDP)),DIRECT',
+        'OR,((DOMAIN-SUFFIX,youtube.com),(GEOSITE,youtube)),Proxy',
+        'NOT,((SRC-IP-CIDR,192.168.1.0/24)),REJECT'
+    ]
+};
 
-try {
-    const fileContents = fs.readFileSync(templatePath, 'utf8');
-    const config = yaml.load(fileContents);
-    const loonConfig = convert(config, {
-        mitm: {
-            p12: 'BASE64P12DATA...',
-            passphrase: '9S25L0J0',
-            hostnames: ['*.example.com']
-        }
-    });
-
-    console.log('--- CONVERSION RESULT ---');
-    console.log(loonConfig);
-    console.log('--- END RESULT ---');
-
-} catch (e) {
-    console.error(e);
-}
+const loonConfig = convert(sampleConfig);
+console.log(loonConfig);
