93d0db0aa362f05536229926e1fa6a887540b3a6
StageGuard <sg@mamoe.net>
2026-01-09 14:13:17 +0800

    print log

--- docker_cmd.sh
+++ docker_cmd.sh
@@ -4,3 +4,4 @@ sudo docker stop mihomo2loon
 sudo docker build -t mihomo2loon .
 sudo docker rm mihomo2loon
 sudo docker run -d --restart=always --name mihomo2loon -p 25500:8080 mihomo2loon
+sudo docker logs mihomo2loon
